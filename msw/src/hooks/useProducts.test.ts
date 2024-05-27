2e98a56019741adc420d16bd0431c33bea083f4c
badahertz52 <badahertz52@gmail.com>
2024-05-27 15:41:41 +0900

    feat: 상품 목록 조회에 대한 로딩, 에러 핸들링 및 폴더 위치 변경

---
+++ msw/src/hooks/useProducts.test.ts
@@ -0,0 +1,39 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import useProducts from "./useProducts";
+
+import { HttpResponse, http } from "msw";
+import { PRODUCTS_ENDPOINT } from "../api/endpoints";
+import { server } from "../mocks/server";
+
+describe("useProducts", () => {
+  describe("상품 목록 조회", () => {
+    it("상품 목록을 조회한다.", async () => {
+      const { result } = renderHook(() => useProducts());
+
+      await waitFor(() => {
+        expect(result.current.products).toHaveLength(20);
+      });
+    });
+
+    it("상품 목록 조회 중 로딩 상태", () => {
+      const { result } = renderHook(() => useProducts());
+
+      expect(result.current.loading).toBe(true);
+    });
+    it("상품 목록 조회 중 에러 상태", async () => {
+      server.use(
+        http.get(PRODUCTS_ENDPOINT, () => {
+          return new HttpResponse(null, { status: 500 });
+        })
+      );
+
+      const { result } = renderHook(() => useProducts());
+
+      await waitFor(() => {
+        expect(result.current.products).toEqual([]);
+        expect(result.current.loading).toBe(false);
+        expect(result.current.error).toBeTruthy();
+      });
+    });
+  });
+});
---
+++ msw/src/hooks/useProducts.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { PRODUCTS_ENDPOINT } from "../api/endpoints";
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  category: string;
+}
+
+interface UseProductsResult {
+  products: Product[];
+  loading: boolean;
+  error: unknown;
+}
+
+export default function useProducts(): UseProductsResult {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<unknown>(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(PRODUCTS_ENDPOINT);
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return { products, loading, error };
+}
